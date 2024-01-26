17d9b1130bfa3021e06efe73a68cbe4885b99185
Maeassar <112930789+Maeassar@users.noreply.github.com>
2024-01-26 21:45:40 +0800

    upload models

--- models/SimpleMultimodel.py
+++ models/SimpleMultimodel.py
@@ -9,28 +9,16 @@ class Model(nn.Module):
 
         self.bert_model = BertModel.from_pretrained("./bert_multilingual") # BERT分支, 用于处理文字
         self.text_trans = nn.Sequential(
-            nn.Dropout(p=0.5),
+            nn.Dropout(p=0.1),
             nn.Linear(768, hidden_size),
             nn.ReLU(inplace=True)
         )
 
-        self.resnet_model = resnet50(pretrained=True) # ResNet分支， 用于处理图片
+        self.resnet_model = resnet50(pretrained=True)
+        self.resnet_fc = nn.Linear(1000, hidden_size)# ResNet分支， 用于处理图片
 
-        self.resnet_fc = nn.Linear(1000, hidden_size)
-        self.resnet_relu = nn.ReLU(inplace=True)
-
-        self.attention = nn.TransformerEncoderLayer(
-            d_model=hidden_size * 2,
-            nhead=8,
-            dropout=0.5
-        )
-
-        self.output = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(hidden_size * 2, hidden_size),
-            nn.ReLU(inplace=True),
-            nn.Linear(hidden_size, num_classes)
-        )
+        self.fusion_layer = nn.Linear(hidden_size * 2, hidden_size) #融合层
+        self.output_layer = nn.Linear(hidden_size, num_classes) #输出层
 
     def forward(self, text_ids, attention_mask, token_type_ids, image_input):
         assert text_ids.shape == attention_mask.shape, "error! text_ids and attention_mask must have same shape!"
@@ -39,13 +27,16 @@ class Model(nn.Module):
                                       token_type_ids=token_type_ids,
                                       attention_mask=attention_mask)["pooler_output"]
         text_output = self.text_trans(text_output)
+        #print("text_output size", text_output.size())
+        #print("text_output pooler_output", text_output)
         image_output = self.resnet_model(image_input)
         image_output = self.resnet_fc(image_output)
+        #print("image_output", image_output)
+        #print("image_output size", image_output.size())
 
-        attention_output = self.attention(
-            torch.cat((text_output, image_output), dim=1)
-        )
+        fused_features = torch.cat((text_output, image_output), dim=1)#使用简单cat
+        fused_features = self.fusion_layer(fused_features)
 
-        output = self.output(attention_output)# 输出层
+        output = self.output_layer(fused_features)# 输出层
 
         return output
